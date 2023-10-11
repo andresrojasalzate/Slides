e0942a51fd2c94c98aee849d43f1020e9184e2d0
= <=>
2023-10-11 19:45:55 +0200

    Creacion fichero vagrant para crear la maquina y la base de datos

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+// Datos de conexión a la base de datos
+define('DB_HOST', '127.0.0.1'); // Dirección del servidor de base de datos
+define('DB_USER', 'usuari'); // Nombre de usuario de la base de datos
+define('DB_PASS', 'usuari'); // Contraseña del usuario de la base de datos
+define('DB_NAME', 'dbGrupo6'); // Nombre de la base de datos
+?>
