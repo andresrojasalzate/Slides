8354c3cf436957583775f8484c6c9e3578b5c475
Diego Yesid Amador Hernandez <dyamador@alumnat.copernic.cat>
2023-10-17 17:36:20 +0000

    Diego

--- src/vista/crearPresentacion.php
+++ src/vista/crearPresentacion.php
@@ -4,20 +4,6 @@ session_start();
 
 require_once '../modelo/Clases/Presentacion.php';
 
-//Almacenar presentaciones desde la BD
-/*$presentaciones = [
-    ['nombre'=> "pres 1",'diapositivas'=>[1,2,3]],
-    ['nombre'=> "pres 2",'diapositivas'=>[1,2,3,4,5,6]],
-    ['nombre'=> "pres 3",'diapositivas'=>[1,2,3,4,5]],
-    ['nombre'=> "pres 4",'diapositivas'=>[1]],
-    ['nombre'=> "pres 5",'diapositivas'=>[1,2]],
-    ['nombre'=> "pres 6",'diapositivas'=>[1,2]],
-    ['nombre'=> "pres 7",'diapositivas'=>[1,2]],
-    ['nombre'=> "pres 8",'diapositivas'=>[1,2]],
-    ['nombre'=> "pres 9",'diapositivas'=>[1,2]],
-    ['nombre'=> "pres 10",'diapositivas'=>[1,2]],
-    ['nombre'=> "pres 11",'diapositivas'=>[1,2]]
-];*/
 
 if (isset($_GET['nombre']) && ($_GET['nombre'] != null && $_GET['nombre'] != "")) {
     $nombre = $_GET['nombre'];
@@ -39,17 +25,13 @@ if (isset($_GET['nombre']) && ($_GET['nombre'] != null && $_GET['nombre'] != "")
 
 <body>
     <form action="../controllers/crearPresentacionController.php" method="post">
-    <div class="divContenForm">
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" name="nombre" required><br>
 
         <label for="descripcion">Descripción:</label>
         <textarea id="descripcion" name="descripcion"></textarea>
-        
-        <div class="containerDreta">
-            <button type="submit" class="botonCrear">Crear</button>
-        </div>
-    </div>
+
+        <button type="submit" class="botonCrear">Crear</button>
     </form>
     
 </body>
