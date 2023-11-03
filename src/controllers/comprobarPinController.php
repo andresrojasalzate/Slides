6831b8e351725c3bf94300675dd9977fe5e8ce10
Diego Yesid Amador Hernandez <dyamador@alumnat.copernic.cat>
2023-11-03 14:46:57 +0000

    ajustes pin y vista cliente 03-11-2023

--- src/controllers/comprobarPinController.php
+++ src/controllers/comprobarPinController.php
@@ -1,38 +1,39 @@
 <?php
+
 use src\modelo\Clases\Presentacion;
+
 require_once '../modelo/Clases/Presentacion.php';
 require_once '../config/ConexionBD.php';
 session_start();
 
-$idPresentacion = 3; //$_SESSION['idPresentacion'];
-$bdConexion = ConexionBD::obtenerInstancia();
-$conexion = $bdConexion->getConnection();
-$pinCorrecto = Presentacion::recuperarPinPresentacion($conexion, $idPresentacion);
-function procesarFormulario($idPresentacion) {
 
+function procesarFormulario()
+{
+    $url = $_SESSION['url'];
+    $bdConexion = ConexionBD::obtenerInstancia();
+    $conexion = $bdConexion->getConnection();
+    $idPres = Presentacion::devolverPresentacionByURL($conexion, $url);
+    $pinCorrecto = Presentacion::recuperarPinPresentacion($conexion, $idPres);
+    $pinIntroducido = $_POST['pin'];
+    $errores = [];
 
-$pinIntroducido = $_POST['pin'];
-$errores = [];
+    if (empty($pinIntroducido)) {
+        $errores['pin'] = "El PIN no puede estar vacio";
+    }
 
-if(empty($pinIntroducido)) {
-    $errores['pin'] = "El PIN no puede estar vacio";
+    if ($pinCorrecto != $pinIntroducido) {
+        $errores['pin'] = "El PIN introducio es incorrecto";
+    }
+
+    if (count($errores) > 0) {
+        $_SESSION['errores'] = $errores;
+        header("Location: ../vista/comprobarPin.php");
+        exit;
+    } else {
+        header("Location: ../vista/vistaCliente.php?url=$url");
+        exit;
+    }
 }
 
-if($pinCorrecto != $pinIntroducido){
-    $errores['pin'] = "El PIN introducio es incorrecto";
-}
-
-if(count($errores) > 0){
-    $_SESSION['errores'] = $errores;
-    header("Location: ../vista/comprobarPin.php");
-    exit;
-} else{
-    header("Location: ../vista/visualizarDiapositiva.php");
-    exit;
-}
-}
-
-
-
-
+procesarFormulario();
 
